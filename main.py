e95ca782facb1ac93a7983710d097ecdeca9081d
sudesh97 <sudesh.kumar290@gmail.com>
2025-11-15 09:38:44 +0530

    validation

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import uvicorn
     # main.py
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-from typing import List
+# from fastapi import FastAPI, Depends
+# from sqlalchemy.orm import Session
+# from typing import List
 
 # from .database import get_db
 # from .models import User
 # from .schemas import UserSchema
-from fastapi import FastAPI
-from pymongo import MongoClient
-client=MongoClient()
+from fastapi import FastAPI, Path
+# from pymongo import MongoClient
+# client=MongoClient()
 app = FastAPI()
 
-client.admin.command("ping") 
+# client.admin.command("ping") 
 
 print("Connected successfully")
 # @app.get("/users/", response_model=List[UserSchema])
 # def read_users(db: Session = Depends(get_db)):
 #     users = db.query(User).all()
 #     return users
+@app.get("/hello1/{name}")
+async def hello1(name:str=Path(...,min_length=3,
+max_length=10)):
+   return {"name": name}
+
+@app.get("/hello/{name}")
+async def hello(name):
+   return {"name": name}
 
 @app.get("/")
 async def index():
